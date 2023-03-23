199546bf1c879769211943f794c25a0ce32c06a9
eunji9128 <80051721+eunji9128@users.noreply.github.com>
2023-03-23 16:01:33 +0900

    Feat/layout main (#1)
    * feat: about page layout
    
    * feat: about page bottom layout
    
    * lib: react material UI carousel installed
    
    * feat: features page layout
    
    * feat: contact comp added into features
    
    * feat: about layout buttom added

---
+++ src/components/About.js
@@ -0,0 +1,120 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+const About = () => {
+    return (
+        <Background>
+            <ShadowBox>
+                <Container>
+                    <Content>
+                        <Vr>
+                            <h1>About me</h1>
+                        </Vr>
+                        <br></br>
+                        <p>I am a junior frontend developer who transitioned from a career in product management.</p>
+                        <hr></hr>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id? Quam recusandae odit fuga ratione corrupti veniam unde laborum dolore, maxime dicta, nemo, eligendi quasi ipsam odio repudiandae iure suscipit?</p>
+                        <Button>
+                            <FontAwesomeIcon icon={solid("download")} style={{color: "#ffffff",}} />
+                            <p>Download CV</p>
+                        </Button>
+                    </Content>
+                    <ProfileImg src="/profile-image.jpg"></ProfileImg>
+                </Container>
+                <Container bgColor="#323232">
+                    <IconBox>
+                        <FontAwesomeIcon icon={brands("square-js")} size="6x" style={{color: "#ffffff",}} />
+                        <h4>JavaScript</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id? Quam recusandae</p>
+                    </IconBox>
+                    <IconBox>
+                        <FontAwesomeIcon icon={brands("react")} size="6x" style={{color: "#ffffff",}} />
+                        <h4>React</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id? Quam recusandae</p>
+                    </IconBox>
+                    <IconBox>
+                        <FontAwesomeIcon icon={brands("html5")} size="6x" style={{color: "#ffffff",}} />
+                        <h4>HTML</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id? Quam recusandae</p>
+                    </IconBox>
+                    <IconBox>
+                        <FontAwesomeIcon icon={brands("css3-alt")} size="6x" style={{color: "#ffffff",}} />
+                        <h4>CSS</h4>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, id? Quam recusandae</p>
+                    </IconBox>
+                </Container>
+            </ShadowBox>
+        </Background>
+    )
+}
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+
+const ShadowBox = styled.div`
+    width: 90vw;
+    height: 100%;
+    box-shadow: 0px 0px 30px grey;
+`
+
+const Container = styled.div`
+    width: 90vw;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: ${props => props.justifyContent || "center"};
+    background-color: ${props => props.bgColor || "white"};
+`
+
+const Content = styled.div`
+    width: 40%;
+    text-align: start;
+    margin: 8%;
+
+    hr {
+        margin-top: 32px;
+        margin-bottom: 32px;
+    }
+`
+
+const Vr = styled.div`
+    border: 4px solid #78BB75;
+    border-width: 0 0 0 4px;
+    padding-left: 16px;
+`
+
+const ProfileImg = styled.img`
+    // src: "/profile-image.jpg";
+    width: 20%;
+    margin: 8%;
+`
+
+const IconBox = styled.div`
+    width: 15%;
+    text-align: center;
+    color: white;
+    margin: 4% 3% 4% 3%;
+`
+
+const Button = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 150px;
+    height: 50px;
+    color: white;
+    background-color: #323232;
+    border: 1px solid grey;
+    margin-top: 4%;
+`
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { brands, solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+const Contact = () => {
+    return (
+        <Container justifyContent="start" margin="4% 10% 4% 10%">
+            <Vr>
+                <Container justifyContent="space-evenly" width="70vw">
+                    <h1>Contact</h1>
+                    <p>+82-10-6212-8912</p>
+                    <p>eunji9128@gmail.com</p>
+                    <FontAwesomeIcon icon={brands("square-github")} size="3x" style={{color: "#000000",}} />
+                    <FontAwesomeIcon icon={solid("envelope")} size="3x" style={{color: "#000000",}} />
+                </Container>
+            </Vr>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    display: flex;
+    align-items: ${props => props.alignItems || "center"};
+    justify-content: ${props => props.justifyContent || "center"};
+    background-color: ${props => props.bgColor || "white"};
+    margin: ${props => props.margin || "0"};
+    flex-direction: ${props => props.direction || "row"};
+    text-align: start;
+`
+
+const Vr = styled.div`
+    width: 90vw;
+    border: 4px solid #78BB75;
+    border-width: 0 0 0 4px;
+    padding-left: 16px;
+`
+
+export default Contact
