914f7883e7a78d7a8cd968b76b5046a75d5239c3
eunji yang <eunji9128@gmail.com>
2023-04-17 22:29:08 +0900

    feat: landing page design and link to home

--- src/Landing.js
+++ src/Landing.js
@@ -1,12 +1,60 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Landing = function () {
     return (
-        <div>
-            <h1>Landing</h1>
-        </div>
+        <Background>
+            <Container>
+                <h1>WELCOME</h1>
+                <Start>
+                    🌒🌓🌔🌕
+                    <Link to="/home"><p>click me!</p></Link>
+                </Start>
+            </Container>
+        </Background>
     )
 };
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(#8ff5fe, #dcb8f6, #0c5ee3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const Container = styled.div`
+    width: 100%;
+    color: #dcb8f6;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    font-size: 5em;
+    text-shadow: 1px 5px 20px #d295ed;
+`
+
+const StyledBtn = styled.button`
+    width: 20vw;
+    height: 10vh;
+    background-color: white;
+    opacity: 0.5;
+    border: none;
+    border-radius: 10px;
+`
+
+const Start = styled.span`
+    text-align: right;
+    margin: 0;
+
+    p {
+        color: #ffffff;
+        font-size: 0.3em;
+        margin: 0;
+        text-decoration: #dcb8f6 wavy underline 4px;
+    }
+`
 
 export default Landing
