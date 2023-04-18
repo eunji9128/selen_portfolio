f677eb8d09a14804c37a2c4d66bbba364162307d
eunji yang <eunji9128@gmail.com>
2023-04-18 18:42:16 +0900

    design: detail page skeleton layout

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -3,8 +3,60 @@ import styled from "styled-components";
 
 const Detail = function () {
     return (
-        <h1>Detail</h1>
+        <Background>
+            <h1>Project Title</h1>
+            <p>project summary blah</p>
+            <Container>
+                <TitleImg></TitleImg>
+            </Container>
+            <Container>
+                <h3>About Project</h3>
+                <p>project details blah blah</p>
+            </Container>
+            <Container>
+                <h3>Technical Sheet</h3>
+                <p>technical sheet blah blah</p>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                </ul>
+            </Container>
+            <Container>
+                <h3>Resources</h3>
+                <p>The project is online at ~~~</p>
+            </Container>
+        </Background>
     )
 };
+
+const Background = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #dcb8f6;
+`
+
+const Container = styled.div`
+    width: 60vw;
+    height: 80vh;
+    background-color: #ffffff;
+    margin-top: 20px;
+    padding: 10px;
+`
+
+const TitleImg = styled.div`
+    width: 100%;
+    height: 60vh;
+    background-image: url("${process.env.PUBLIC_URL}/feature1.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+`
 
 export default Detail
