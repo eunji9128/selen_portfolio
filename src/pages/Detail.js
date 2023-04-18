43953ce79c94efce4b79a5bf59e79503d2879b59
eunji yang <eunji9128@gmail.com>
2023-04-18 17:45:02 +0900

    etc: change branch name & add routes for detail page

---
+++ src/pages/Detail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Detail = function () {
+    return (
+        <h1>Detail</h1>
+    )
+};
+
+export default Detail
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import MoonAnime from "./components/MoonAnime";
+import MoonAnime from "../components/MoonAnime";
 
 const Landing = function () {
     return (
@@ -54,9 +54,5 @@ const StyledLink = styled(Link)`
         text-decoration: none;
     }
 `
-
-
-
-
 
 export default Landing
