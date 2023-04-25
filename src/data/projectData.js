a996202e876c177ea71d9f20d20e68eed35a15ce
eunji yang <eunji9128@gmail.com>
2023-04-25 19:09:27 +0900

    refact: add 404 redirecting & modify features content

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,7 +2,7 @@ let projectData = [
     {
         "id": 0,
         "name": "robot delivery app - personal project",
-        "summary": "뉴빌리티 - 로봇 배달 앱 MVP TF 프로덕트 매니징",
+        "summary": "뉴빌리티 - 로봇 배달 앱 MVP TF 프로덕트 매니징(비즈니스 및 서비스 기획/운영, MVP 앱 런칭 후 시범 서비스 운영)",
         "tech": [
             "Figma",
             "Axure",
@@ -14,7 +14,7 @@ let projectData = [
     {
         "id": 1,
         "name": "shoes online mall - personal project",
-        "summary": "개인 프로젝트 - 온라인 신발 판매 웹 사이트 개발",
+        "summary": "개인 프로젝트 - 온라인 신발 판매 웹 사이트 개발(API 데이터 연동, local storage 장바구니 기능, React router 활용)",
         "tech": [
             "JavaScript",
             "React",
@@ -25,16 +25,16 @@ let projectData = [
             "styled-components",
             "FontAwesome"
         ],
-        "resource": "https://eunji9128.github.io"
+        "resource": "https://eunji9128.github.io/shoesmall"
     },
     {
         "id": 2,
         "name": "Responsive Landing Page - personal project",
-        "summary": "개인 프로젝트 - 반응형 랜딩 페이지 개발",
+        "summary": "개인 프로젝트 - 반응형 랜딩 페이지 개발(mobile device 반응형 레이아웃, 애니메이션, HTML/CSS 퍼블리싱)",
         "tech": [
-            "JavaScript",
             "HTML5",
             "CSS3",
+            "Bootstrap",
             "FontAwesome"
         ],
         "resource": "https://eunji9128.github.io/dm_w_LandingPage"
@@ -42,11 +42,11 @@ let projectData = [
     {
         "id": 3,
         "name": "Responsive SNS Profile Page - personal project",
-        "summary": "개인 프로젝트 - 반응형 SNS 프로파일 페이지 개발",
+        "summary": "개인 프로젝트 - 반응형 SNS 프로파일 페이지 개발(mobile device 반응형 레이아웃, HTML/CSS 퍼블리싱)",
         "tech": [
-            "JavaScript",
             "HTML5",
             "CSS3",
+            "Bootstrap",
             "FontAwesome"
         ],
         "resource": "https://eunji9128.github.io/dm_w_SNSprofile"
